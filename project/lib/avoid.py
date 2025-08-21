be0c4e34347e37e257dce74c2224a744912d8217
Leo Projceski <126206757+Leo-Proj@users.noreply.github.com>
2025-08-21 14:29:11 +1000

    big commit
    i forgot to commit

--- project/lib/avoid.py
+++ project/lib/avoid.py
@@ -6,26 +6,34 @@ from PiicoDev_Unified import sleep_ms
 from navigation import Navigation
 
 #instantiation
-r_servo_pwm = PWM(Pin(16))
-l_servo_pwm = PWM(Pin(15))
 
-l_servo = Servo(pwm=l_servo_pwm)
-r_servo = Servo(pwm=r_servo_pwm)
 
-range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-range_b = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+class Avoid:
+    def __init__(self, debug=False):
+        self.__debug = debug
 
-movement = Navigation(l_servo, r_servo, debug=True)
+    r_servo_pwm = PWM(Pin(16))
+    l_servo_pwm = PWM(Pin(15))
 
-def avoid():
-    while True:
-        distance1 = range_a.distance_mm
-        distance2 = range_b.distance_mm
-        if distance1 <= 100 and distance2 > 100:
-            movement.move_r()
-        elif distance1 > 100 and distance2 <= 100:
-            movement.move_forward()
-        elif distance1 <= 100 and distance2 <= 100:
-            movement.move_l()
-        else:
-            movement.move_forward()
+    l_servo = Servo(pwm=l_servo_pwm)
+    r_servo = Servo(pwm=r_servo_pwm)
+
+    range_a = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+    range_b = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+
+    movement = Navigation(l_servo, r_servo, debug=True)
+
+    def avoid(self):
+        if self.__debug:
+            print("Testing obstacle detection")
+        while True:
+            distance1 = range_a.distance_mm
+            distance2 = range_b.distance_mm
+            if distance1 <= 100 and distance2 > 100:
+                movement.move_r()
+            elif distance1 > 100 and distance2 <= 100:
+                movement.move_forward()
+            elif distance1 <= 100 and distance2 <= 100:
+                movement.move_l()
+            else:
+                movement.move_forward()
--- project/lib/controller.py
+++ project/lib/controller.py
@@ -1,8 +1,43 @@
+from navigation import Navigation
+from colour import Colour
+from screen import Screen
+from avoid import Avoid
+from avoid import movement # didnt know u could do this but the internet is a lovely place
+from PiicoDev_Unified import sleep_ms
 
-
-
-class Controller:
-    def __init__(self):
-
+class Controller: # goliath
+    def __init__(self, debug=False):
+        self.__debug = debug
+        self.__avoider = Avoid()
+        self.__colour = Colour()
+        self.__screen = Screen()
+        self.__movement = movement
 
     def update(self):
+        while True:
+            green = self.__colour.find_green()
+            if green:
+                self.__screen.display_stuff("Green :)")
+                self.__movement.stop()
+                self.__movement.spin()
+                self.__movement.move_forward()
+
+            distance1 = self.__avoider.range_a.dista    nce_mm
+            distance2 = self.__avoider.range_b.distance_mm
+            
+            if distance1 <= 100 and distance2 > 100:
+                self.__movement.move_r()
+                self.__screen.display_stuff("Moving Right")
+            elif distance1 > 100 and distance2 <= 100:
+                self.__movement.move_forward()
+                self.__screen.display_stuff("Moving Forward")
+            elif distance1 <= 100 and distance2 <= 100:
+                self.__movement.move_l()
+                self.__screen.display_stuff("Moving Left")
+            else:
+                self.__movement.move_forward()
+                self.__screen.display_stuff("Moving Forward")
+
+if __name__ == "__main__":
+    robot = Controller()
+    robot.update()
